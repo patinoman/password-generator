57dc6fd6a304aa3942ebbeafa904c3b94aaa691b
patinoman <68838876+patinoman@users.noreply.github.com>
2020-08-23 22:18:31 -0700

    do/while loop validated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var inputFlag = true;
+var characterType = true;
 
 var generateBtn = document.querySelector("#generate");
 
@@ -17,21 +18,31 @@ function generatePassword() {
 
   // uppercaseCharacters [1]
   do{
-    inputFlag = true
+    inputFlag = true;
     var numCharacter = prompt ("How many characters would you like your password to contain? (8-128)");
       numCharacter = parseInt (numCharacter);
 
-    if (numCharacter < 8 || numCharacter > 128 || isNaN(parseInt)){
+    if (numCharacter < 8 || numCharacter > 128 || isNaN(numCharacter)){
         alert("Please select valid entry");
         inputFlag = false;  
       }
+      console.log(inputFlag);
   } while (inputFlag === false);
 
-    var characters = confirm ("Click OK to confirm including special characters");
-    var numbers = confirm ("Click OK to confirm including numeric characters");
-    var uppercases = confirm ("Click OK to confirm including uppercase characters");
-    var lowercases = confirm ("Click OK to confirm including lowercase characters");
-  
+
+
+    do{
+        characterType = true;
+        var characters = confirm ("Click OK to confirm including special characters");
+        var numbers = confirm ("Click OK to confirm including numeric characters");
+        var uppercases = confirm ("Click OK to confirm including uppercase characters");
+        var lowercases = confirm ("Click OK to confirm including lowercase characters");
+
+        if (characters === false && numbers === false && uppercases === false && lowercases === false){
+            characterType = false;
+            alert("Please make a selection");
+        }
+    } while (characterType === false);
     
     
 
